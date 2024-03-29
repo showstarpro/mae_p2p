ac47a1eaafe6fccc764612f542ce4f9893809578
showstarpro <1359947339@qq.com>
2024-03-29 20:30:15 +0800

    update lamda

---
+++ fintune.sh
@@ -0,0 +1,11 @@
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=8 main_finetune.py \
+    --accum_iter 2 \
+    --batch_size 64 \
+    --model vit_base_patch16 \
+    --finetune /home/aiscuser/mae_p2p_3_11/mae/p2p_vitb_09/checkpoint-299.pth \
+    --epochs 100 \
+    --blr 5e-4 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
+    --dist_eval --data_path /home/dataset/ImageNet \
+    --output_dir ./p2p_vitb_09_ft\
+    --log_dir ./p2p_vitb_09_ft \
---
+++ pretrain.sh
@@ -0,0 +1,28 @@
+data_path=/mnt/ImageNet/
+
+python -m torch.distributed.launch --nproc_per_node=4 main_pretrain.py \
+    --batch_size 2 \
+    --accum_iter 4 \
+    --model mae_vit_base_patch16 \
+    --norm_pix_loss \
+    --mask_ratio 0.75 \
+    --epochs 300 \
+    --warmup_epochs 40 \
+    --blr 1.5e-4 --weight_decay 0.05 \
+    --data_path ${data_path} \
+    --output_dir ./p2p_vitb_15\
+    --log_dir ./p2p_vitb_15 \
+    --lamda 1.5 \
+    # --distributed
+
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=8 main_finetune.py \
+    --accum_iter 2 \
+    --batch_size 64 \
+    --model vit_base_patch16 \
+    --finetune /home/aiscuser/mae_p2p_3_11/mae/p2p_vitb_15/checkpoint-299.pth \
+    --epochs 100 \
+    --blr 5e-4 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
+    --dist_eval --data_path ${data_path} \
+    --output_dir ./p2p_vitb_15_ft\
+    --log_dir ./p2p_vitb_15_ft \
